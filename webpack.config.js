e5b8e57ede02d7349496c35eb2e8e6aa03c26ca9
Maksym Solomko <104863585+maxexc@users.noreply.github.com>
2024-07-10 07:08:59 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build'),
         clean: true,
-        publicPath: '/',
+        publicPath: '/uCard/',
     },
     devServer: {
         static: {
